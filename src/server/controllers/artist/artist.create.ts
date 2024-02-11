c2975b2a36a86097ee8c3c1219a1b28228b3f243
Angelo Lucas <Angelolucasfl@gmail.com>
2024-02-10 22:48:06 -0300

    Adding first controller

--- src/server/controllers/artist/artist.create.ts
+++ src/server/controllers/artist/artist.create.ts
@@ -1,13 +1,30 @@
 import { Request, Response } from "express";
+// import { StatusCodes } from "http-status-codes"; 
+import * as yup from "yup";
 
 
 interface IArtist {
   nome: string;
 }
 
-export const create = (req: Request<{}, {}, IArtist>, res: Response) => {
-  const data = req.body;
-  console.log(data);
+const bodyValidation: yup.Schema<IArtist> = yup.object().shape({
+  nome: yup.string().required().min(1)
+});
 
-  return res.send("created");
+export const create = async (req: Request<{}, {}, IArtist>, res: Response) => {
+  let validatedData: IArtist | undefined = undefined;
+
+  try{
+    validatedData = await bodyValidation.validate(req.body);
+  } catch (error) {
+    const yupError = error as yup.ValidationError;
+
+    return res.json({
+      errors: {
+        default: yupError.message
+      }
+    });
+  }
+
+  return res.send(validatedData.nome);
 };
