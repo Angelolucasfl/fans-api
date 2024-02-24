afbccf37d5b2ab3596f968a24cdf0105ffa16f53
Angelo Lucas <Angelolucasfl@gmail.com>
2024-02-24 14:46:10 -0300

    pg settings

--- src/server/database/knex/index.ts
+++ src/server/database/knex/index.ts
@@ -1,6 +1,12 @@
 import knex from "knex";
+import pg from "pg";
+import "dotenv/config";
 import { development, test, production } from "./Environment";
 
+
+if(process.env.NODE_ENV === "production"){
+  pg.types.setTypeParser(20, "text", parseInt);
+}
 
 const getEnv = () => {
   switch(process.env.NODE_ENV){
