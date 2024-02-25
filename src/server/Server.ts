2d8a7a8954e1bf30f4e901cc981fcf30d6471640
Angelo Lucas <Angelolucasfl@gmail.com>
2024-02-25 20:19:10 -0300

    swagger remove

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -3,9 +3,6 @@ import "./shared/services/translations.yup";
 import "dotenv/config";
 import { router } from "./routes";
 import cors from "cors";
-import swaggerUi from "swagger-ui-express";
-import swaggerDocs from "../swagger.json";
-import path from "path";
 
 
 export class Server {
@@ -23,12 +20,6 @@ export class Server {
     }));
     this.server.use(express.json());
     this.server.use(express.urlencoded({ extended: true }));
-    this.server.get("/api-docs-ui/swagger-ui.css", (req, res) => {
-      res.setHeader("Content-Type", "text/css");
-      res.sendFile(path.join(__dirname, "public/swagger-ui.css"));
-    });
-
-    this.server.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
   }
 
   routes(): void {
